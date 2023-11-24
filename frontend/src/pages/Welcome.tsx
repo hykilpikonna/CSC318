14143cfeb3565ef7bea91b912e8004bc767190dd
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-11-24 13:51:54 -0500

    [S] Style welcome page

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,9 +1,15 @@
+import DuoSplash from "../assets/img/duo-splash.png";
+
 export default function Welcome()
 {
-  return <>
-    <div>Duolingo++</div>
+  return <div className="flex flex-col h-screen justify-center">
+    <div className="flex flex-col p-5 gap-5 items-center">
+      <img src={DuoSplash} alt="Duolingo Logo"></img>
 
-    <button>Login</button>
-    <button>Signup</button>
-  </>
+      <h1>Duolingo Enhanced</h1>
+
+      <button className="green">Login</button>
+      <button className="white">Signup</button>
+    </div>
+  </div>
 }
