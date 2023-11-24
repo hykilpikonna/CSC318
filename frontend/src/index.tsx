ebfbccb9f4dee6fde0d81cc92a5151ed2ceb9b05
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-11-24 13:20:57 -0500

    [U] Router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.sass';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Welcome from "./pages/Welcome";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Welcome/>,
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
