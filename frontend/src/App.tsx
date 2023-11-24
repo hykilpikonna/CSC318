e9fc775b3a8abe63002dc8f0e816ccd00cc1520e
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-11-24 12:28:16 -0500

    [+] Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.sass';
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 function App() {
-  return (
+  return <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/about"></Route>
+      <Route path="/users"></Route>
+      <Route path="/"></Route>
     </div>
-  );
+  </Router>
 }
 
 export default App;
